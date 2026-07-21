eb894f57bd74202ff4a94dd268dbd5921d1109ff
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2026-07-21 13:07:52 +0530

    solve temperature conversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+let num;
+const convertToCelsius = function(num) {
+  let res = ((num - 32) * (5 / 9));
+  return Math.round(res * 10)/ 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let res = ((num * (9/5)) + 32);
+  return Math.round(res * 10)/ 10;
 };
 
 // Do not edit below this line
