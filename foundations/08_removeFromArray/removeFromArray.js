caa632b5260ea5c5b85099f2780ffa7c4587ce02
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2026-07-21 12:34:39 +0530

    solve remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+let arr;
+const removeFromArray = function (arr) {
+    for (let i = 1; i < arguments.length; i += 1) {
+        let index = arr.indexOf(arguments[i])
+        while (index !== -1) {
+            arr.splice(index, 1); 
+            index = arr.indexOf(arguments[i]);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
