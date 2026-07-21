9eace5cdf6ed7072a5a37e7b4bc6766a17baf45c
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2026-07-21 11:51:08 +0530

    solve repeat string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+let str;
+let num;
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    let res = ""
+    for (let i = 1; i <= num; i += 1){
+        res += str;
+    }
+    return res;
 };
 
 // Do not edit below this line
