3c0a2781cc9dcc093d593f5d98e9d5aca7f73283
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2026-07-22 14:40:26 +0530

    solve calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+let a;
+let b;
+let arr;
+
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) { 
+  const res = arr.reduce((summ, value) => { 
+    return summ += value; 
+  }, 0); 
+  return res; 
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  const res = arr.reduce((product, value) => {
+    return product *= value;
+  }, 1)
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let res = 1;
+	for (let i = 1; i <= a; i += 1){
+    res *= i
+  }
+  return res
 };
 
 // Do not edit below this line
