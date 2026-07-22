0f97e395d390013d723421c45ba3a4e8c6104cc0
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2026-07-22 15:09:35 +0530

    solve find the oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+let arr;
+const findTheOldest = function(arr) {
+    for (let person of arr){
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    }
+    arr.sort((a, b) => {
+        const ageA = a.yearOfDeath - a.yearOfBirth
+        const ageB = b.yearOfDeath - b.yearOfBirth
+        return ageB - ageA
+    })
+    return arr[0]
 };
 
 // Do not edit below this line
