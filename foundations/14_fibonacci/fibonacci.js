a569e383047c3805b6f0dca79a63f94c755a0191
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2026-07-22 14:56:57 +0530

    solve fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+let num;
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    if (num == 2) return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
