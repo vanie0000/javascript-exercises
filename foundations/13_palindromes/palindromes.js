de00bc89621cd3c86d29b72278df3d15bce807dc
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2026-07-22 14:50:02 +0530

    solve palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const isAlnum = (char) => { 
+  const code = char.charCodeAt(0); 
+  return (code > 47 && code < 58) || 
+         (code > 64 && code < 91) ||  
+         (code > 96 && code < 123); 
+}; 
 
-};
+const palindromes = function(str) { 
+  let newStr = str.toLowerCase(); 
+  let res = ""; 
+  
+  for (const char of newStr){ 
+    if (isAlnum(char)) res += char; 
+  } 
+  
+  let left = 0; 
+  let right = res.length - 1; 
+  
+  while (left <= right){ 
+    if (res[left] !== res[right]) {
+      return false; 
+    }
+    left++;
+    right--;
+  } 
+  return true; 
+}; 
 
-// Do not edit below this line
 module.exports = palindromes;
